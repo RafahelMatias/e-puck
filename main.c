1a2f6b25744445d3a312b93a66d875cd65fc0d6f
Rafahel Matias <rafahelcosta@gmail.com>
2026-02-18 22:13:07 -0300

    Update main

--- main.c
+++ main.c
@@ -1,192 +1,126 @@
-#include "p30F6014A.h" 
- 
-#define IR_RECIEVER 
- 
-#include <stdio.h> 
-#include "string.h" 
-#include "math.h" 
-#include <time.h> 
- 
-#include < library\motor_led\e_init_port.h> 
- 
-7  
-#include < C:\library\motor_led/advance_one_timer/e_led.h> 
-#include < C:\ library\motor_led/advance_one_timer/e_motors.h> 
-#include <C: \library\motor_led/advance_one_timer/e_agenda.h> 
- 
- 
-#include <C:\ library\a_d/advance_ad_scan/e_ad_conv.h> 
-#include <C:\ library\a_d/advance_ad_scan/e_prox.h> 
-#include <C:\ library\a_d/advance_ad_scan/e_acc.h> 
-#include <C:\ library\a_d/advance_ad_scan/e_micro.h> 
-#include <C:\ library\motor_led/e_epuck_ports.h> 
-#include <C:\ library\acc_gyro/e_lsm330.h> 
-#include <C:\ library\I2C/e_I2C_protocol.h> 
- 
-/* selector on normal extension*/ 
- 
-#define SELECTOR0 _RG6 
-#define SELECTOR1 _RG7 
-#define SELECTOR2 _RG8 
-#define SELECTOR3 _RG9 
- 
-#include "C:\ MPLABXProjects\e-puck-library-master\program\DemoGCtronic-complete\memory.h" 
- 
-char buffer[BUFFER_SIZE]; 
- 
-extern int selector; 
-char c; 
- 
-#ifdef IR_RECIEVER 
- 
-#define SPEED_IR 600 
-#endif  
- 
- 
-#define PI 3.14159265358979 
-#define ARRAY_WIDTH 640 
-#define ARRAY_HEIGHT 480 
- 
- 
-int main()  
-{ 
-   
-  e_init_port();           
-  e_start_agendas_processing(); 
-  e_init_motors(); 
-     
-         
-     
-         
-  // Para selecionar a chave seletora 
-  selector=getselector(); 
- 
-  int max_motor_speed = 1000; 
- 
-     
-  if (selector==0) { // ------------------------------------------------------------------------------------------------------------------------ 
- 
-      //  Colocar uma função   
-         
- 
-            
-  } else if (selector==1) { // ---------------------------------------------------------------------------------------------------------------- 
- 
-      //  Colocar uma função   
-         
- 
-         
-    } else if (selector==2){ 
- 
- 
-      //  Colocar uma função   
-         
-            
-    } else if (selector==3){ // ------------------------------------------------------------------------------------------------------------------- 
-       
-//  Colocar uma função   
-         
-       
-    } else if (selector == 4){ // ------------------------------------------------------------------------------------------------------------------- 
-        
-       
-//  Colocar uma função   
-         
-         
-    } else if (selector == 5){ // ------------------------------------------------------------------------------------------------------------------- 
-       
-//  Colocar uma função   
-         
- 
-    } else if (selector == 6){ 
-       
-//  Colocar uma função   
-         
-    } 
-     
-       
-  while(1); 
-  return 0; 
-}
+/*
+ * Projeto: Controle e-puck - Iniciação Científica
+ * Autor: Rafahel Matias
+ * Descrição: Algoritmos de movimentação autônoma e enxame.
+ */
 
-int anda_robo(int speed) { 
-     
-  e_set_speed_right(speed); 
-  e_set_speed_left(speed);  
-   
-  return 0;
-}
+#include "p30F6014A.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+// Imports das bibliotecas do e-puck
+#include "e-puck-library-master/library/motor_led/e_init_port.h"
+#include "e-puck-library-master/library/motor_led/advance_one_timer/e_led.h"
+#include "e-puck-library-master/library/motor_led/advance_one_timer/e_motors.h"
+#include "e-puck-library-master/library/motor_led/advance_one_timer/e_agenda.h"
+#include "e-puck-library-master/library/a_d/advance_ad_scan/e_ad_conv.h"
+#include "e-puck-library-master/library/a_d/advance_ad_scan/e_prox.h"
+#include "e-puck-library-master/library/motor_led/e_epuck_ports.h"
+
+// Definições de Hardware
+#define SELECTOR0 _RG6
+#define SELECTOR1 _RG7
+#define SELECTOR2 _RG8
+#define SELECTOR3 _RG9
+#define PI 3.14159265358979
 
 
+int getselector(void); 
+void anda_robo(int speed);
+void vira(float speed, float prop_esq, float prop_dir);
+void anda_para(int sensor_id, int dist_limite, int vel, int tempo_wait);
+void anda_sala(int sensor_id, int dist_limite, int vel, int tempo_wait);
+void controle_leds(int led_id, int estado);
 
-int vira(float speed, float prop_esq, float prop_dir) { 
-     
-    e_set_speed_right(speed * prop_esq); 
-    e_set_speed_left(speed * prop_dir);  
-     
+int main() {
+    // 1. Inicialização do Sistema
+    e_init_port();
+    e_start_agendas_processing();
+    e_init_motors();
+
+    // 2. Leitura do Seletor
+    int selector = getselector();
+    int max_speed = 1000;
+
+    // 3. Loop Principal 
+    while (1) {
+        switch (selector) {
+            case 0:
+                
+                anda_robo(0);
+                break;
+            case 1:
+                // Exemplo: Andar e Parar se ver obstáculo
+                // Sensor 0 (frente esquerda), Distancia 35, Vel 500
+                anda_para(0, 35, 500, 10000); 
+                break;
+            case 2:
+                // Exemplo: Comportamento de Sala
+                anda_sala(0, 100, 600, 10000);
+                break;
+            case 3:
+                // Teste de LEDs
+                controle_leds(0, 1); // Acende LED 0
+                wait(50000);
+                controle_leds(0, 0); // Apaga LED 0
+                wait(50000);
+                break;
+            default:
+                // Comportamento padrão se seletor inválido
+                controle_leds(8, 1); // Acende todos os LEDs
+                break;
+        }
+    }
     return 0;
-  
 }
 
 
 
-int anda_para(int sensor, int dist, int vel, int temp){
-  int valor_sensor;
-  while (1) {
-    valor_sensor = e_get_prox(sensor);
-    // Se o valor lido pelo sensor for maior que 35, pare o robô
-    if (valor_sensor > dist){
-    e_set_speed_left(0);
-    e_set_speed_right(0); 
-               
-    } else { 
-      // Caso contrário, continue movendo o robô para frente (ou faça o que desejar) 
-      e_set_speed_left(vel);   
-
-      e_set_speed_right(vel);  
-    } 
-
-    wait(temp); 
-    } 
-       
-
-  return 0;
+void anda_robo(int speed) {
+    e_set_speed_right(speed);
+    e_set_speed_left(speed);
 }
 
-
-int anda_sala(int vel, int temp){ 
-     
-  int valor_sensor;  
-        
-  while (1) { 
-        
-    valor_sensor = e_get_prox(sensor); 
-
-    if (valor_sensor > dist) 
-    { 
-      e_set_speed_left(vel/2); 
-      e_set_speed_right(-vel/2); 
-      // aqui pode ser colocada outras funcionalidades para caso o robô tenha essa leitura do sensor 
+void vira(float speed, float prop_esq, float prop_dir) {
     
-    } else { 
-      e_set_speed_left(vel);   
-
-      e_set_speed_right(vel);  
-    }  
-  // É possível também adicionar outros ifs aqui com condicionais diferentes utilizando a câmera e/ou os sensores 
-
-  
-  wait(temp); 
-  } 
-   
+    e_set_speed_right((int)(speed * prop_esq));
+    e_set_speed_left((int)(speed * prop_dir));
 }
 
-int LEDs(int x, int y) { 
-  // Função que exemplifica o uso dos LEDs do robô
-     
-  e_set_led (x,y),   //x é o número do LED no robô e y é o nível logico. 
-  e_set_front_led (y),     //acende um LED frontal com luz mais forte 
-  e_blink_led (),     //acende todos os LEDs
-   
-  return 0; 
+void anda_para(int sensor_id, int dist_limite, int vel, int tempo_wait) {
+    int valor_sensor = e_get_prox(sensor_id);
+
+    // Se o obstáculo estiver muito perto (valor alto), para.
+    if (valor_sensor > dist_limite) {
+        e_set_speed_left(0);
+        e_set_speed_right(0);
+    } else {
+        e_set_speed_left(vel);
+        e_set_speed_right(vel);
+    }
+    wait(tempo_wait);
+}
+
+
+void anda_sala(int sensor_id, int dist_limite, int vel, int tempo_wait) {
+    int valor_sensor = e_get_prox(sensor_id);
+
+    if (valor_sensor > dist_limite) {
+        // Gira no próprio eixo se encontrar obstáculo
+        e_set_speed_left(vel / 2);
+        e_set_speed_right(-vel / 2);
+    } else {
+        // Segue reto
+        e_set_speed_left(vel);
+        e_set_speed_right(vel);
+    }
+    wait(tempo_wait);
+}
+
+void controle_leds(int led_id, int estado) {
+    e_set_led(led_id, estado);   
+    e_set_front_led(estado);     
+    e_blink_led();
 }
